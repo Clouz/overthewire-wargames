486ba40c457762857e6aa82be908eeba30ccd2ee
Claudio Mola <clouz85@gmail.com>
2017-11-02 20:37:58 +0300

    Delete natas14.bash

--- Natas/natas14.bash
+++
@@ -1,7 +0,0 @@
-usr="natas14"
-psw="Lg96M10TdfaPyVBkJdjymbllQ5L6qdl1"
-
-user1="ciao"
-psw1="ciao"
-
-curl --user "$usr:$psw" http://$usr.natas.labs.overthewire.org/?debug=1&username=$user1&password=$psw1 
